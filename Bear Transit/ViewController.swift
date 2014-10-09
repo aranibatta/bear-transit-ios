1f6143b5e54302d5e228e3b46a50b19d12d52198
Daylen Yang <services@daylenyang.com>
2014-10-09 11:37:37 -0700

    Initial Commit

---
+++ Bear Transit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bear Transit
+//
+//  Created by Daylen Yang on 10/9/14.
+//  Copyright (c) 2014 Daylen Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
