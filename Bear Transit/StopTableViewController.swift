f544120b3a7fc9c4c1ee37d414b6e30ba51b4661
Daylen Yang <services@daylenyang.com>
2014-10-13 22:12:25 -0700

    - Display ETA for buses - Display alert if no buses are running

--- Bear Transit/StopTableViewController.swift
+++ Bear Transit/StopTableViewController.swift
@@ -26,17 +26,40 @@ class StopTableViewController: UITableViewController {
                 println(error)
                 AlertUtils.showAlert(self, title: "Connection error", message: "\(error?.description)")
             } else {
-                self.populateNextBusCells(json as Dictionary<String, AnyObject>)
+                self.saveNextBusCellsData(json as Dictionary<String, AnyObject>)
             }
         }
     }
     
-    func populateNextBusCells(info : Dictionary<String, AnyObject>) {
+    func saveNextBusCellsData(info : Dictionary<String, AnyObject>) {
         var next: AnyObject? = info["next"] as AnyObject?
         if let next: AnyObject = next {
             departures = next as [Dictionary<String, AnyObject>]
             tableView.reloadData()
+            if departures.count == 0 {
+                AlertUtils.showAlert(self, title: "No active lines", message: "No Bear Transit lines are running right now.")
+            }
         }
+    }
+    
+    func getTimeDifference(time : Int) -> String {
+        let calendar = NSCalendar(calendarIdentifier: NSGregorianCalendar)
+        
+        let now = NSDate()
+        let components = calendar.components(NSCalendarUnit.YearCalendarUnit|NSCalendarUnit.MonthCalendarUnit|NSCalendarUnit.DayCalendarUnit, fromDate: now)
+        components.hour = time / 100
+        components.minute = time % 100
+        let future = calendar.dateFromComponents(components)
+        
+        let diff = calendar.components(NSCalendarUnit.HourCalendarUnit|NSCalendarUnit.MinuteCalendarUnit, fromDate: now, toDate: future!, options: .allZeros)
+        let hours = diff.hour
+        let mins = diff.minute
+        if (hours > 0) {
+            return "\(hours)h \(mins)m"
+        } else {
+            return "\(mins) min"
+        }
+        
     }
     
     override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
@@ -59,8 +82,9 @@ class StopTableViewController: UITableViewController {
             cell.lineName.text = (departures[indexPath.row]["line"]! as String)
             let destination = departures[indexPath.row]["line_note"]! as String
             cell.lineNote.text = "To \(destination)"
-            cell.timeOne.text = "LOL"
-            cell.timeTwo.text = "2"
+            let times = departures[indexPath.row]["times"]! as [Int]
+            cell.timeOne.text = getTimeDifference(times.first!)
+            cell.timeTwo.text = times.count > 1 ? getTimeDifference(times[1]) : ""
             return cell
         }
     }
